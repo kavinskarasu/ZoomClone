f40608c329ff089f3bc3b26a74a906c44e8af590
kavinskarasu <kavinskarasu@gmail.com>
2022-11-26 21:25:32 +0530

    updated

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import {Grid,Typography,Paper} from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import { SocketContext } from '../SocketContext'
 
+const useStyles = makeStyles((theme) => ({
+    video: {
+      width: '550px',
+      [theme.breakpoints.down('xs')]: {
+        width: '300px',
+      },
+    },
+    gridContainer: {
+      justifyContent: 'center',
+      [theme.breakpoints.down('xs')]: {
+        flexDirection: 'column',
+      },
+    },
+    paper: {
+      padding: '10px',
+      border: '2px solid black',
+      margin: '10px',
+    },
+  }));
 const VideoPlayer=()=>{
+    const classes=useStyles();
     return (
-        <div>
-            VideoPlayer
-        </div>
+       <Grid container className={classes.gridContainer}>
+  <Paper  className={classes.paper}>
+    <Grid item xs={12} md={6}>
+<Typography variant='hs' gutterBottom>Name</Typography>
+<Video playsInline muted ref={myVideo} autoPlay className={classes.video}/>
+    </Grid>
+  </Paper>
+       </Grid>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,13 @@ import ReactDom from 'react-dom';
  
 
 import App from './App'
+import { contextProvider } from './SocketContext';
 import './styles.css'
+
+ReactDom.render(
+    <contextProvider>
+        <App/>
+    </contextProvider>
+)
 
 ReactDom.render(<App/>,document.getElementById('root'))
