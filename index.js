80c2d6484d52536eb04b05c6fdd5c0bd41d98f4b
kavinskarasu <kavinskarasu@gmail.com>
2022-11-26 17:36:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const  app=require('express')();
+const server=require('http').createServer(app);
+const cors=require('cors')
+
+const io=require("socket.io")(server,{
+    cores:{
+        origin:'*',
+        methods:["GET","POST"]
+    }
+});
+
+app.use(cors());
+
+const PORT=process.env.PORT ||5000;
+
+
+app.get("/",(req,res)=>{
+    res.send("server is running ");
+})
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
